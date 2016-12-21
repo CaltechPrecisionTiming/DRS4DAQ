c712d79917fcc7ac575ae399cecf1fa3c3cb9690
Jiajing Mao <jiajing.mao@cern.ch>
2016-12-21 15:51:40 -0800

    update

--- DT5742/testBinary.cc
+++ DT5742/testBinary.cc
@@ -71,7 +71,7 @@ int main()
 		   //channel[j][i] = buffer[i+6+j*1024];
 		  raw[j][i] = buffer[i+6+j*1024];
 		  channel[j][i] = ( raw[j][i] - 2047 )/4096 ;
-		  std::cout << "i = " << i << " ; j = " << j << " ; raw[j][i] = " << raw[j][i] << " ; channel[j][i] = " << channel[j][i] << std::endl; 
+		  //std::cout << "i = " << i << " ; j = " << j << " ; raw[j][i] = " << raw[j][i] << " ; channel[j][i] = " << channel[j][i] << std::endl; 
 		}
 	    }
 	  tree->Fill();	
@@ -120,11 +120,55 @@ int main()
 
   TH1F *tr1 = new TH1F("tr1","tr1",1024,0,1024);	
 
+  TH1F *channel0 = new TH1F("channel0","channel0",1024,-0.5,0.5);
+  TH1F *channel1 = new TH1F("channel1","channel1",1024,-0.5,0.5);
+  TH1F *channel2 = new TH1F("channel2","channel2",1024,-0.5,0.5);
+  TH1F *channel3 = new TH1F("channel3","channel3",1024,-0.5,0.5);
+  TH1F *channel4 = new TH1F("channel4","channel4",1024,-0.5,0.5);
+  TH1F *channel5 = new TH1F("channel5","channel5",1024,-0.5,0.5);
+  TH1F *channel6 = new TH1F("channel6","channel6",1024,-0.5,0.5);
+  TH1F *channel7 = new TH1F("channel7","channel7",1024,-0.5,0.5);
+  TH1F *channel8 = new TH1F("channel8","channel8",1024,-0.5,0.5);
+  TH1F *channel9 = new TH1F("channel9","channel9",1024,-0.5,0.5);
+  TH1F *channel10 = new TH1F("channel10","channel10",1024,-0.5,0.5);
+  TH1F *channel11 = new TH1F("channel11","channel11",1024,-0.5,0.5);
+  TH1F *channel12 = new TH1F("channel12","channel12",1024,-0.5,0.5);
+  TH1F *channel13 = new TH1F("channel13","channel13",1024,-0.5,0.5);
+  TH1F *channel14 = new TH1F("channel14","channel14",1024,-0.5,0.5);
+  TH1F *channel15 = new TH1F("channel15","channel15",1024,-0.5,0.5);
+  TH1F *channel16 = new TH1F("channel16","channel16",1024,-0.5,0.5);
+  TH1F *channel17 = new TH1F("channel17","channel17",1024,-0.5,0.5);
+
+
   double x;
+  double y ;
   int ctr = 1;
-  buffer = buffer + 6 ;  
- 
-  while(ctr>0 && ctr<1024+1) { x = *(buffer+ctr); ch0->SetBinContent(ctr,x); ctr++;}
+  float* buffer1;
+  buffer1 = buffer + 6 ;  
+
+  while(ctr>0 && ctr<1024+1) { x = *(buffer1+ctr); ch0->SetBinContent(ctr,x);  y = (x-2047)/4096 ;  channel0->SetBinContent(ctr,y);  ctr++;}
+  while(ctr>1024 && ctr<1024*2+1) { x = *(buffer1+ctr); ch1->SetBinContent(ctr-1024,x);  y = (x-2047)/4096 ;  channel1->SetBinContent(ctr,y);  ctr++;}
+  while(ctr>1024*2 && ctr<1024*3+1) { x = *(buffer1+ctr); ch2->SetBinContent(ctr-1024*2,x);  y = (x-2047)/4096 ;  channel2->SetBinContent(ctr,y); ctr++;}
+  while(ctr>1024*3 && ctr<1024*4+1) { x = *(buffer1+ctr); ch3->SetBinContent(ctr-1024*3,x);  y = (x-2047)/4096 ;  channel3->SetBinContent(ctr,y); ctr++;}
+  while(ctr>1024*4 && ctr<1024*5+1) { x = *(buffer1+ctr); ch4->SetBinContent(ctr-1024*4,x);  y = (x-2047)/4096 ;  channel4->SetBinContent(ctr,y);  ctr++;}
+  while(ctr>1024*5 && ctr<1024*6+1) { x = *(buffer1+ctr); ch5->SetBinContent(ctr-1024*5,x);  y = (x-2047)/4096 ;  channel5->SetBinContent(ctr,y);  ctr++;}
+  while(ctr>1024*6 && ctr<1024*7+1) { x = *(buffer1+ctr); ch6->SetBinContent(ctr-1024*6,x);  y = (x-2047)/4096 ;  channel6->SetBinContent(ctr,y);  ctr++;}
+  while(ctr>1024*7 && ctr<1024*8+1) { x = *(buffer1+ctr); ch7->SetBinContent(ctr-1024*7,x);  y = (x-2047)/4096 ;  channel7->SetBinContent(ctr,y);  ctr++;}
+
+  while(ctr>1024*8 && ctr<1024*9+1) { x = *(buffer1+ctr); tr0->SetBinContent(ctr-1024*8,x);  y = (x-2047)/4096 ;  channel8->SetBinContent(ctr,y);  ctr++;}
+
+  while(ctr>1024*9 && ctr<1024*10+1) { x = *(buffer1+ctr); ch8->SetBinContent(ctr-1024*9,x);  y = (x-2047)/4096 ;  channel9->SetBinContent(ctr,y);  ctr++;}
+  while(ctr>1024*10 && ctr<1024*11+1) { x = *(buffer1+ctr); ch9->SetBinContent(ctr-1024*10,x);  y = (x-2047)/4096 ;  channel10->SetBinContent(ctr,y);  ctr++;}
+  while(ctr>1024*11 && ctr<1024*12+1) { x = *(buffer1+ctr); ch10->SetBinContent(ctr-1024*11,x);  y = (x-2047)/4096 ;  channel11->SetBinContent(ctr,y);  ctr++;}
+  while(ctr>1024*12 && ctr<1024*13+1) { x = *(buffer1+ctr); ch11->SetBinContent(ctr-1024*12,x);  y = (x-2047)/4096 ;  channel12->SetBinContent(ctr,y);  ctr++;}
+  while(ctr>1024*13 && ctr<1024*14+1) { x = *(buffer1+ctr); ch12->SetBinContent(ctr-1024*13,x);  y = (x-2047)/4096 ;  channel13->SetBinContent(ctr,y);  ctr++;}
+  while(ctr>1024*14 && ctr<1024*15+1) { x = *(buffer1+ctr); ch13->SetBinContent(ctr-1024*14,x);  y = (x-2047)/4096 ;  channel14->SetBinContent(ctr,y);  ctr++;}
+  while(ctr>1024*15 && ctr<1024*16+1) { x = *(buffer1+ctr); ch14->SetBinContent(ctr-1024*15,x);  y = (x-2047)/4096 ;  channel15->SetBinContent(ctr,y);  ctr++;}
+  while(ctr>1024*16 && ctr<1024*17+1) { x = *(buffer1+ctr); ch15->SetBinContent(ctr-1024*16,x);  y = (x-2047)/4096 ;  channel16->SetBinContent(ctr,y);  ctr++;}
+
+  while(ctr>1024*17 && ctr<1024*18+1) { x = *(buffer1+ctr); tr1->SetBinContent(ctr-1024*17,x);  y = (x-2047)/4096 ;  channel17->SetBinContent(ctr,y);  ctr++;}
+/* 
+  while(ctr>0 && ctr<1024+1) { x = *(buffer+ctr); ctr++;}
   while(ctr>1024 && ctr<1024*2+1) { x = *(buffer+ctr); ch1->SetBinContent(ctr-1024,x); ctr++;}
   while(ctr>1024*2 && ctr<1024*3+1) { x = *(buffer+ctr); ch2->SetBinContent(ctr-1024*2,x); ctr++;}
   while(ctr>1024*3 && ctr<1024*4+1) { x = *(buffer+ctr); ch3->SetBinContent(ctr-1024*3,x); ctr++;}
@@ -144,8 +188,8 @@ int main()
   while(ctr>1024*15 && ctr<1024*16+1) { x = *(buffer+ctr); ch14->SetBinContent(ctr-1024*15,x); ctr++;}
   while(ctr>1024*16 && ctr<1024*17+1) { x = *(buffer+ctr); ch15->SetBinContent(ctr-1024*16,x); ctr++;}
 	
-  while(ctr>1024*17 && ctr<1024*18+1) { tr1->SetBinContent(ctr-1024*17,x); ctr++;}
-
+  while(ctr>1024*17 && ctr<1024*18+1) { x = *(buffer+ctr); tr1->SetBinContent(ctr-1024*17,x); ctr++;}
+*/
   tree->Write();
   f->Write();
   f->Close();

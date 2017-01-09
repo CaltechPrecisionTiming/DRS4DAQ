762ca48a61ba8ff015ed894a5354381b7f68cb8e
Jiajing Mao <jiajing.mao@cern.ch>
2017-01-09 15:45:35 -0800

    including functions

---
+++ DT5742/src/Aux.cc
@@ -0,0 +1,352 @@
+#include "Aux.hh"
+
+
+//*********************************************************
+// Get amplification factor used for the silicon sensor
+//*********************************************************
+double GetAmplificationFactor ( double measuredAmplitude ) {
+  
+  int index_firstBinAboveInput = -1;
+  for (int i=0; i < nPoints; ++i) {
+    index_firstBinAboveInput = i;
+    if (measuredAmplitude < outputAmplitude[i]) break;
+  }
+  
+  double answer = 0; 
+
+  if (measuredAmplitude > outputAmplitude[21]) answer =amplificationFactor[21];
+  else if (index_firstBinAboveInput == 0) answer = amplificationFactor[0];
+  else {
+    
+    //cout << "index_firstBinAboveInput = " << index_firstBinAboveInput << " : "
+    //	 << amplificationFactor[index_firstBinAboveInput-1] << " " << outputAmplitude[index_firstBinAboveInput]
+    //	 << "\n";
+    double x = measuredAmplitude - outputAmplitude[index_firstBinAboveInput-1];
+    double y = amplificationFactor[index_firstBinAboveInput-1] + x * (amplificationFactor[index_firstBinAboveInput] - amplificationFactor[index_firstBinAboveInput-1]) / (outputAmplitude[index_firstBinAboveInput] - outputAmplitude[index_firstBinAboveInput-1]);
+    //cout << "x = " << x << " , y = " << y << "\n";
+    answer = y;
+  }
+
+  //cout << measuredAmplitude << " " << answer << "\n";
+
+  return answer;
+  
+}
+
+
+TGraphErrors* GetTGraph(  float* channel, float* time )
+{		
+  //Setting Errors
+  float errorX[1024], errorY[1024], channelFloat[1024];
+  float _errorY = 0.00; //5%error on Y
+  for ( int i = 0; i < 1024; i++ )
+    {
+      errorX[i]       = .0;
+      errorY[i]       = _errorY*channel[i];
+      channelFloat[i] = -channel[i];
+    }
+  TGraphErrors* tg = new TGraphErrors( 1024, time, channelFloat, errorX, errorY );
+  return tg;
+};
+
+
+TGraphErrors* GetTGraph(  short* channel, float* time )
+{		
+  //Setting Errors
+  float errorX[1024], errorY[1024], channelFloat[1024];
+  float _errorY = 0.00; //5%error on Y
+  for ( int i = 0; i < 1024; i++ )
+    {
+      errorX[i]       = .0;
+      errorY[i]       = _errorY*channel[i];
+      channelFloat[i] = -channel[i];
+    }
+  TGraphErrors* tg = new TGraphErrors( 1024, time, channelFloat, errorX, errorY );
+  return tg;
+};
+
+////////////////////////////////////////////
+// find minimum of the pulse
+// aa added protection against pulses with single high bin
+////////////////////////////////////////////
+int FindMin( int n, short *a) {
+  
+  if (n <= 0 || !a) return -1;
+  float xmin = a[5];
+  int loc = 0;
+  for  (int i = 5; i < n-5; i++) {
+    if (xmin > a[i] && a[i+1] < 0.5*a[i])  {
+      xmin = a[i];
+      loc = i;
+    }
+  }
+  
+  return loc;
+}
+
+int FindMin( int n, float *a) {
+  
+  if (n <= 0 || !a) return -1;
+  float xmin = a[5];
+  int loc = 0;
+  for  (int i = 5; i < n-5; i++) {
+    if (xmin > a[i] && a[i+1] < 0.5*a[i])  {
+      xmin = a[i];
+      loc = i;
+    }
+  }
+  
+  return loc;
+}
+
+// find the mean time from gaus fit
+float GausFit_MeanTime(TGraphErrors* pulse, const float index_first, const float index_last)
+{
+  TF1* fpeak = new TF1("fpeak","gaus", index_first, index_last);
+  pulse->Fit("fpeak","Q","", index_first, index_last);
+  
+  float timepeak = fpeak->GetParameter(1);
+  delete fpeak;
+  
+  return timepeak;
+};
+
+float GausFit_MeanTime(TGraphErrors* pulse, const float index_first, const float index_last, TString fname)
+{
+  TF1* fpeak = new TF1("fpeak","gaus", index_first, index_last);
+  //float max = pulse->GetMaximum();
+  double max = -9999;
+  double* yy = pulse->GetY();
+  for ( int i = 0; i < 1024; i++ )
+    {
+      if ( yy[i] > max ) max = yy[i];
+    }
+  //std::cout << "max: " << max << std::endl;
+  if( max < 42 || index_first < 10 || index_last > 1010 ) return -99999;
+  pulse->Fit("fpeak","Q","", index_first, index_last);
+  
+  TCanvas* c = new TCanvas("canvas","canvas",800,400) ;
+  float timepeak = fpeak->GetParameter(1);
+  pulse->GetXaxis()->SetLimits( timepeak-10, timepeak+10);
+  pulse->SetMarkerSize(1);
+  pulse->SetMarkerStyle(20);
+  pulse->Draw("AP");
+  c->SaveAs(fname+".pdf");
+  delete fpeak;
+  
+  return timepeak;
+};
+
+float RisingEdgeFitTime(TGraphErrors * pulse, const float index_min, const float constantFraction, TString fname, bool makePlot )
+{
+  double x_low, x_high, y, dummy;
+  //pulse->GetPoint(index_min-7, x_low, y);
+  //pulse->GetPoint(index_min-2, x_high, y);
+  pulse->GetPoint(index_min-12, x_low, y);
+  pulse->GetPoint(index_min-7, x_high, y);
+  pulse->GetPoint(index_min, dummy, y);  
+  TF1* flinear = new TF1("flinear","[0]*x+[1]", x_low, x_high );
+  
+  pulse->Fit("flinear","Q","", x_low, x_high );
+  double slope = flinear->GetParameter(0);
+  double b     = flinear->GetParameter(1);
+  
+  if ( makePlot )
+    {
+      std::cout << "make plot" << std::endl;
+      TCanvas* c = new TCanvas("canvas","canvas",800,400) ;
+      pulse->GetXaxis()->SetLimits(x_low-3, x_high+3);
+      pulse->SetMarkerSize(1);
+      pulse->SetMarkerStyle(20);
+      pulse->Draw("AP");
+      c->SaveAs(fname+"LinearFit.pdf");
+      //delete c;
+    }
+  delete flinear;
+  
+  return (constantFraction*y-b)/slope;
+};
+
+
+void RisingEdgeFitTime(TGraphErrors * pulse, const float index_min, float* tstamp, TString fname, bool makePlot )
+{
+  double x_low, x_high, y, dummy;
+  pulse->GetPoint(index_min-6, x_low, y);
+  pulse->GetPoint(index_min-2, x_high, y);
+  //pulse->GetPoint(index_min-12, x_low, y);
+  //pulse->GetPoint(index_min-7, x_high, y);
+  pulse->GetPoint(index_min, dummy, y);
+  
+  TF1* flinear = new TF1("flinear","[0]*x+[1]", x_low, x_high );
+  float max = -9999;
+  double* yy = pulse->GetY();
+  
+  for ( int i = 0; i < 1024; i++ )
+    {
+      if ( yy[i] > max ) max = yy[i];
+    }
+  //std::cout << "max: " << max << std::endl;
+  if( max < 42 || index_min < 10 || index_min > 1010 ) return;
+  
+  pulse->Fit("flinear","Q","", x_low, x_high );
+  double slope = flinear->GetParameter(0);
+  double b     = flinear->GetParameter(1);
+  
+  if ( makePlot )
+    {
+      std::cout << "make plot" << std::endl;
+      TCanvas* c = new TCanvas("canvas","canvas",800,400) ;
+      pulse->GetXaxis()->SetLimits(x_low-3, x_high+3);
+      pulse->SetMarkerSize(1);
+      pulse->SetMarkerStyle(20);
+      pulse->Draw("AP");
+      c->SaveAs(fname+"LinearFit.pdf");
+      //delete c;
+    }
+  tstamp[0] = (0.0*y-b)/slope;
+  //std::cout << "----" << tstamp[0]  << std::endl;
+  tstamp[1] = (0.15*y-b)/slope;
+  tstamp[2] = (0.30*y-b)/slope;
+  tstamp[3] = (0.45*y-b)/slope;
+  tstamp[4] = (0.60*y-b)/slope;
+  
+  delete flinear;
+};
+
+
+double GetGaussTime( TGraphErrors* pulse )
+{
+  return 0;
+};
+
+
+float GetBaseline( int peak, short *a ) {
+
+  float tmpsum = 0;
+  float tmpcount = 0;
+  //std::cout << "GGG\n";
+  if (peak < 300) {
+    for  (int i = peak + 200; i < 1000; i++) {
+      // std::cout << i << " : " << a[i] << "\n";
+      tmpsum += a[i];
+      tmpcount += 1.0;
+    }
+  } else {
+    for  (int i = 5; i < peak-200; i++) {
+      // std::cout << i << " : " << a[i] << "\n";
+      tmpsum += a[i];
+      tmpcount += 1.0;
+    }
+  }
+  // std::cout << tmpsum / tmpcount << "\n";
+
+  return tmpsum / tmpcount;
+}
+
+float GetBaseline( int peak, float *a ) {
+
+  float tmpsum = 0;
+  float tmpcount = 0;
+  //std::cout << "GGG\n";
+  if (peak < 300) {
+    for  (int i = peak + 200; i < 1000; i++) {
+      // std::cout << i << " : " << a[i] << "\n";
+      tmpsum += a[i];
+      tmpcount += 1.0;
+    }
+  } else {
+    for  (int i = 5; i < peak-200; i++) {
+      // std::cout << i << " : " << a[i] << "\n";
+      tmpsum += a[i];
+      tmpcount += 1.0;
+    }
+  }
+  // std::cout << tmpsum / tmpcount << "\n";
+
+  return tmpsum / tmpcount;
+}
+
+
+float GetPulseIntegral(int peak, short *a) 
+{
+  float integral = 0.;
+
+  for (int i=245; i < 295; i++) {
+    integral += a[i] * 0.2 * 1e-9 * (1.0/4096.0) * (1.0/50.0) * 1e12; //in units of pC, for 50Ohm termination
+  }
+
+  return -1.0 * integral;
+}
+
+TGraphErrors* GetTGraphFilter( short* channel, float* time, TString pulseName, bool makePlot )
+{
+  float Gauss[1024];
+  //Setting Errors
+  float errorX[1024], errorY[1024], channelFloat[1024];
+  float _errorY = 0.00; //5%error on Y
+  
+  for ( int i = 0; i < 1024; i++ )
+    {
+      
+      errorX[i]       = .0;
+      errorY[i]       = _errorY*channel[i];
+      channelFloat[i] = -channel[i];
+    }
+  
+  TF1 *fb = new TF1("fb","gaus(0)", 0.0, 204.6);
+  fb->SetParameter(1, 100);
+  float sigma =1.0;
+  fb->SetParameter(2, sigma);
+  fb->SetParameter(0, 1/(sqrt(3.1415*2.0)*sigma) );
+  //eval Gaussian
+  float step = 0.2;//200ps
+  for ( int i = 0; i < 1024; i++ )
+    {
+      Gauss[i] = fb->Eval( float(i)*step );
+    }
+  
+  float channelFloatFiltered[2048];
+  for ( int i = 0; i < 2048; i++ )
+    {
+      float convolvedPoint = 0;
+      for ( int j = 0; j <= i; j++ )
+	{
+	  if ( i < 1024 )
+	    {
+	      convolvedPoint += channelFloat[i-j]*Gauss[1023-j];
+	    }
+	  else
+	    {
+	      if ( 1023-(i-1023)-j >= 0 ) convolvedPoint += channelFloat[1023-j]*Gauss[1023-(i-1023)-j];
+	    }
+	}
+      //if ( i < 1024 ) channelFloatFiltered[i] = convolvedPoint;
+      channelFloatFiltered[i] = convolvedPoint;
+    }
+  
+  float channelFloatFilteredFix[1024];
+  for ( int i = 0; i < 1024; i++ )
+    {
+      channelFloatFilteredFix[i] = 0.2*channelFloatFiltered[i+523];
+    }
+  
+  TGraphErrors* tg = new TGraphErrors( 1024, time, channelFloat, errorX, errorY );
+  TGraphErrors* tg2 = new TGraphErrors( 1024, time, channelFloatFilteredFix, errorX, errorY );
+
+  if (makePlot) {
+    TCanvas* c = new TCanvas("canvas","canvas",800,400) ;         
+    tg2->GetXaxis()->SetLimits(50, 70);
+    tg->GetXaxis()->SetLimits(50, 70);
+    //tg2->Fit("fb","","", 0.0, 204.6 );
+    tg2->SetMarkerSize(0.5);
+    tg->SetMarkerSize(0.5);
+    tg2->SetMarkerStyle(20);
+    tg->SetMarkerStyle(20);
+    tg2->Draw("AP");
+    tg2->SetMarkerColor(kBlue);
+    tg->Draw("sameP");
+    c->SaveAs(pulseName + "GausPulse.pdf");
+  }
+  return tg;
+};
+
